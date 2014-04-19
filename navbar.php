77dfcb2c0df7b3ece849b2f7869f61e13ae78a17
Christopher Roy Bratusek <nano@tuxfamily.org>
2014-04-18 21:06:35 -0300

    Menu e navbar adicionados e semi-configurados, pagina index mais limpa

---
+++ navbar.php
@@ -0,0 +1,66 @@
+<!-- /navbar -->
+<div class="subnavbar">
+  <div class="subnavbar-inner">
+    <div class="container">
+      <ul class="mainnav">
+        <li class="active"><a href="index.php"><i class="icon-dashboard"></i><span>Dashboard</span> </a> </li>
+
+
+        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> 
+          <i class=" icon-group"></i><span>Assistência CRAS</span> <b class="caret"></b></a>
+
+          <ul class="dropdown-menu">
+            <li><a href="icons.php">Icons</a></li>
+            <li><a href="faq.php">FAQ</a></li>
+            <li><a href="pricing.php">Pricing Plans</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="signup.php">Signup</a></li>
+            <li><a href="error.php">404</a></li>
+          </ul>
+        </li>
+        
+        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> 
+          <i class="icon-user"></i><span>RH</span> <b class="caret"></b></a>
+
+          <ul class="dropdown-menu">
+            <li><a href="icons.php">Icons</a></li>
+            <li><a href="faq.php">FAQ</a></li>
+            <li><a href="pricing.php">Pricing Plans</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="signup.php">Signup</a></li>
+            <li><a href="error.php">404</a></li>
+          </ul>
+        </li>
+
+        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> 
+          <i class="icon-list-alt"></i><span>Relatórios</span> <b class="caret"></b></a>
+
+          <ul class="dropdown-menu">
+            <li><a href="icons.php">Icons</a></li>
+            <li><a href="faq.php">FAQ</a></li>
+            <li><a href="pricing.php">Pricing Plans</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="signup.php">Signup</a></li>
+            <li><a href="error.php">404</a></li>
+          </ul>
+        </li>
+
+        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> 
+          <i class="icon-money"></i><span>Financeiro</span> <b class="caret"></b></a>
+
+          <ul class="dropdown-menu">
+            <li><a href="icons.php">Icons</a></li>
+            <li><a href="faq.php">FAQ</a></li>
+            <li><a href="pricing.php">Pricing Plans</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="signup.php">Signup</a></li>
+            <li><a href="error.php">404</a></li>
+          </ul>
+        </li>
+
+      </ul>
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /subnavbar-inner --> 
+</div>
---
+++ top.php
@@ -0,0 +1,31 @@
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
+                    class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="index.php">Sistema Único de Assistência ao CRAS - SUAC </a>
+      <div class="nav-collapse">
+        <ul class="nav pull-right">
+          <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
+                            class="icon-cog"></i> Ajustes <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="javascript:;">Configurações</a></li>
+              <li><a href="javascript:;">Ajuda</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
+                            class="icon-user"></i> Marcos Ellys <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="javascript:;">Perfil</a></li>
+              <li><a href="login.php">Logout</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="navbar-search pull-right">
+          <input type="text" class="search-query" placeholder="Pesquisar">
+        </form>
+      </div>
+      <!--/.nav-collapse --> 
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /navbar-inner --> 
+</div>
